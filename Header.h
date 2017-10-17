f5b00a19ae566da16dd1583ae165f3e932fa59ae
Fedeleo94 <30713560+Fedeleo94@users.noreply.github.com>
2017-10-17 06:19:48 +0200

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+#include <unistd.h>
+
+
+int Log_In (char*,char*);
+int Create_User (char*,char*);
+int String_Compare (char*,char*);
+int String_Lenght (char*);
